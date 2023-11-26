9dc4bde156f15cf513485edc5f753935a5b5af1f
trelea <example@trelea.com>
2023-11-26 21:16:32 +0200

    update controllers/posts/searchPosts.js

--- controllers/posts/searchPosts.js
+++ controllers/posts/searchPosts.js
@@ -10,18 +10,22 @@ FUCKING LONG SQL QUERY
 post_title LIKE $1 OR 
 post_title LIKE UPPER($1) OR 
 post_title LIKE LOWER($1) OR 
+post_title LIKE INITCAP($1) OR
 
 post_title LIKE $2 OR 
 post_title LIKE UPPER($2) OR 
 post_title LIKE LOWER($2) OR
+post_title LIKE INITCAP($2) OR
 
 post_title LIKE $3 OR 
 post_title LIKE UPPER($3) OR 
 post_title LIKE LOWER($3) OR
+post_title LIKE INITCAP($3) OR
 
 post_title LIKE $4 OR 
 post_title LIKE UPPER($4) OR 
 post_title LIKE LOWER($4) 
+post_title LIKE INITCAP($4) OR
 
 ORDER BY id DESC LIMIT 100", [post_title, post_title + '%', '%' + post_title, '%' + post_title + '%']
 
@@ -31,7 +35,7 @@ ORDER BY id DESC LIMIT 100", [post_title, post_title + '%', '%' + post_title, '%
 module.exports = async (req, res) => {
 	const { post_title } = req.params;
 	try {
-		let postSearched = await db.query("SELECT post_id, post_title, user_name, post_thumbnail FROM posts WHERE post_title LIKE $1 OR post_title LIKE UPPER($1) OR post_title LIKE LOWER($1) OR post_title LIKE $2 OR post_title LIKE UPPER($2) OR post_title LIKE LOWER($2) OR post_title LIKE $3 OR post_title LIKE UPPER($3) OR post_title LIKE LOWER($3) OR post_title LIKE $4 OR post_title LIKE UPPER($4) OR post_title LIKE LOWER($4) ORDER BY id DESC LIMIT 100", [post_title, post_title + '%', '%' + post_title, '%' + post_title + '%']);
+		let postSearched = await db.query("SELECT post_id, post_title, user_name, post_thumbnail FROM posts WHERE post_title LIKE $1 OR post_title LIKE UPPER($1) OR post_title LIKE LOWER($1) OR post_title LIKE INITCAP($1) OR post_title LIKE $2 OR post_title LIKE UPPER($2) OR post_title LIKE LOWER($2) OR post_title LIKE INITCAP($2) OR post_title LIKE $3 OR post_title LIKE UPPER($3) OR post_title LIKE LOWER($3) OR post_title LIKE INITCAP($3) OR post_title LIKE $4 OR post_title LIKE UPPER($4) OR post_title LIKE LOWER($4) OR post_title LIKE INITCAP($4) ORDER BY id DESC LIMIT 100", [post_title, post_title + '%', '%' + post_title, '%' + post_title + '%']);
 		postSearched = postSearched.rows
 		return res.status(200).json({
 			postSearched
