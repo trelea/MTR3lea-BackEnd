1222146421b03f08a8c8967684a69a70e35bb373
trelea <treleamarius76@gmail.com>
2024-01-08 21:08:48 +0200

    jan 8 2024 commit

--- controllers/posts/getPosts.js
+++ controllers/posts/getPosts.js
@@ -7,18 +7,17 @@ module.exports = async (req, res) => {
 
     if ( (limit === 20 || limit === 50 || limit === 100) && (page >= 1) ) {
 
-        if (limit * page > (await db.query("SELECT COUNT(id) FROM posts")).rows[0].count) return res.status(201).json({ msg: "You are too far"}).end();
-
         const fullLimit = limit;
         const fullOffset = (page - 1) * fullLimit;
         const sqlQuery = "SELECT t1.post_id, t1.user_name, t2.user_thumbnail, t1.post_title, t1.post_thumbnail, t1.post_likes, t1.post_comments, t1.post_created_at, t1.post_updated_at FROM posts t1 JOIN users t2 ON t1.user_name = t2.user_name ORDER BY t1.id DESC LIMIT $1 OFFSET $2";
         const values = [fullLimit, fullOffset];
         const SQL = await db.query(sqlQuery, values);
 
-
         let Posts = SQL.rows;
         Posts = JSON.stringify(Posts).replaceAll("array_length", "post_likes");
         Posts = JSON.parse(Posts);
+
+        if (Posts.length <= 0)  return res.status(201).json({ msg: "You are too far"}).end();
 
         return res.status(200).json({
             Posts
